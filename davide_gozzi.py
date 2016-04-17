1f98cde11770e6bb1da1e519b436f4575fb1077a
g4solio <daddidaffi@gmail.com>
2016-04-17 10:07:44 +0200

    Added files via upload

---
+++ davide_gozzi.py
@@ -0,0 +1,17 @@
+from microbit import *
+code = {
+        '-----': '0',  '.----': '1',  '..---': '',
+        '...--': '3',  '....-': '4',  '.....': '5',
+        '-....': '6',  '--...': '7',  '---..': '8',
+        '----.': '9'
+        }
+while(button_a.get_presses()+button_b.get_presses()<5):
+    a=''
+    if(button_a.is_pressed()):
+        a=a+'.'
+    else(butto_b.is_pressed()):
+        a=a+'-'
+
+
+b=code[a]
+display.scroll(b)
